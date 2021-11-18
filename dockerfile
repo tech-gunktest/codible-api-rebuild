541f0d43a0ba65ed114666a50f8ef89c44450efe
tech-gunktest <94447223+tech-gunktest@users.noreply.github.com>
2021-11-18 12:13:09 +0530

    Add files via upload

---
+++ dockerfile
@@ -0,0 +1,65 @@
+FROM node:carbon
+WORKDIR /usr/src/app
+COPY package*.json ./
+COPY index.js ./
+COPY file.html ./
+RUN apt-get update  \
+	&& apt-get install -y \
+# Base Packages
+	tzdata software-properties-common apt-transport-https wget git curl vim nano zip sudo net-tools xvfb php npm supervisor gnupg \
+# C, C++
+	build-essential \
+# C-Sharp
+	mono-complete \
+# Clojure
+	clojure \
+# Java
+	default-jre \
+	default-jdk \
+# Lua
+	lua5.3 \
+# Perl
+	perl \
+	perl6 \
+# Python
+	python3 \
+	python3-pip \
+# R
+	r-base \
+# Ruby
+	ruby  \
+# Scala
+	&& wget https://www.scala-lang.org/files/archive/scala-2.13.6.deb -P /tmp  \
+	&& apt install -y /tmp/scala-2.13.6.deb  \
+# Yarn
+	&& npm install --global yarn  \
+# Go
+	&& bash -c 'echo "Installing Go..."'  \
+	&& wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz -P /tmp  \
+	&& tar -zxvf /tmp/go1.16.4.linux-amd64.tar.gz -C /tmp > /dev/null 2>&1  \
+	&& cp -r /tmp/go/ /usr/local  \
+	&& bash -c 'echo "Go Installed!"'  \
+# rust
+	&& curl https://sh.rustup.rs -sSf | sh -s -- -y  \
+	&& sudo chmod -R a+w $RUSTUP_HOME $CARGO_HOME \
+	&& cargo --version \
+# Nodejs(LTS Release)
+	&& bash -c 'echo "Installing Nodejs..."'  \
+	&& wget https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz -P /tmp  \
+	&& tar -xvf /tmp/node-v14.17.0-linux-x64.tar.xz -C /tmp > /dev/null 2>&1  \
+	&& cp -r /tmp/node-v14.17.0-linux-x64/* /usr \
+	&& bash -c 'echo "Installed Nodejs!"' \
+# PowerShell
+	&& wget -q https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -P /tmp  \
+	&& apt install -y  /tmp/packages-microsoft-prod.deb  \
+	&& apt-get update  \
+	&& apt-get install -y powershell  \
+# Swift
+	&& bash -c 'echo "Installing Swift..."'  \
+	&& wget https://swift.org/builds/swift-5.4.1-release/ubuntu2004/swift-5.4.1-RELEASE/swift-5.4.1-RELEASE-ubuntu20.04.tar.gz -P /tmp  \
+	&& tar -zxvf /tmp/swift-5.4.1-RELEASE-ubuntu20.04.tar.gz -C /tmp > /dev/null 2>&1  \
+	&& cp -r /tmp/swift-5.4.1-RELEASE-ubuntu20.04/usr/* /usr  \
+	&& bash -c 'echo "Swift Installed!"'  \
+RUN npm install --only=production
+EXPOSE $PORT
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+process = require('child_process');
+fs = require('fs');
+
+app.use(express.urlencoded({ extended: false }))// server to understand what app data is
+app.use(express.json())
+
+function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/file.html')
+})
+
+app.post('/compile/python', (req, res)=>{
+    const code = req.body.code
+    const input = req.body.input
+    const args = ['-c', code]
+    const py = process.spawn('python3', args)
+if(input){
+    py.stdin.write(input)
+    py.stdin.end()
+}
+    py.stdout.on('data', (data)=>{
+        res.send(data.toString())
+    })
+})
+
+app.post('/compile/node', (req, res)=>{
+    var f= getRandomString(16)+'.js'
+    if(fs.existsSync(f)){
+        f= getRandomString(8)+'.js'
+    }else{
+    fs.writeFile(f, req.body.code, (err)=>{
+        if(err) throw err;
+    })
+}
+    const node = process.spawn('node', [f])
+    node.stdout.on('data', (data)=>{
+        res.send(data.toString())
+    })
+});
+
+app.post('/compile/ruby', (req, res)=>{
+    var f= getRandomString(16)+'.rb'
+    if(fs.existsSync(f)){
+        f= getRandomString(8)+'.rb'
+    }else{
+    fs.writeFile(f, req.body.code, (err)=>{
+        if(err) throw err;
+    })
+}
+    const ruby = process.spawn('ruby', [f])
+    ruby.stdout.on('data', (data)=>{
+        res.send(data.toString())
+    })
+});
+
+app.post('/compile/perl', (req, res)=>{
+    var f= getRandomString(16)+'.pl'
+    if(fs.existsSync(f)){
+        f= getRandomString(8)+'.pl'
+    }else{
+    fs.writeFile(f, req.body.code, (err)=>{
+        if(err) throw err;
+    })
+}
+    const perl = process.spawn('perl', [f])
+    perl.stdout.on('data', (data)=>{
+        res.send(data.toString())
+    })
+});
+
+app.listen(4000, function(){
+    console.log('listening on http://localhost:4000')
+})
